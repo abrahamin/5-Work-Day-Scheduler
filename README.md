65b08cc2f83da2aee504bb741204973c9c4422eb
Abraham Hong <hhmfjg@gmail.com>
2021-09-24 19:58:03 -0400

    javascript functionality

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 5-Work-Day-Scheduler
+
+## Description
+
+The purpose of this project was to create a simple calendar that displays each work hour of the day, where the user can save events.
+The application was made to run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Moment.js was used to display the date and time.
+
+In order to meet the Acceptance Criteria of the project:
+* the planner displays the current day at the top
+* timeblocks for standard business hours are color coded to indicate what is past, the present, or in the future
+* events entered in the timeblock can be saved by clicking the save button
+
+## Screenshot
+
+![Work Day Scheduler](/assets/images/scheduler.png)
+
+## Link
+
+[Work Day Scheduler](https://abrahamin.github.io/5-Work-Day-Scheduler/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,14 @@ var four = moment(16, "H").isBefore();
 var five = moment(17, "H").isBefore();
 
 var times = [nine, ten, eleven, twelve, one, two, three, four, five];
-console.log(times);
 
 var hourNow = moment().format("h");
-console.log(hourNow);
 
 var hoursInDay = $('span');
-console.log(hoursInDay[0].innerHTML);
 
 var textArea = $('textarea');
 textArea.each( function(i){
+    var newValue = $(this).parent().attr("id");
     past = times[i];
     current = hoursInDay[i].innerHTML;
     if (current == hourNow) {
@@ -30,44 +28,12 @@ textArea.each( function(i){
         $(this).toggleClass("past");
     } else {
         $(this).toggleClass("future");
-    };
+    }
+    $(this).val(localStorage.getItem(newValue));
 });
 
-var block1 = $('#block1');
-var block2 = $('#block2');
-var block3 = $('#block3');
-var block4 = $('#block4');
-var block5 = $('#block5');
-var block6 = $('#block6');
-var block7 = $('#block7');
-var block8 = $('#block8');
-var block9 = $('#block9');
-
-var blockArray = [block1[0], block2[0], block3[0], block4[0], block5[0], block6[0], block7[0], block8[0], block9[0]];
-
-var button1 = $('#button1');
-var button2 = $('#button2');
-var button3 = $('#button3');
-var button4 = $('#button4');
-var button5 = $('#button5');
-var button6 = $('#button6');
-var button7 = $('#button7');
-var button8 = $('#button8');
-var button9 = $('#button9');
-
-var buttonArray = [button1[0], button2[0], button3[0], button4[0], button5[0], button6[0], button7[0], button8[0], button9[0]];
-
-console.log(buttonArray);
-
-console.log(blockArray);
-
-button1[0].addEventListener("click", function() {
-    console.log("button-clicked")
-})
-
-for (var i = 0; i < buttonArray.length; i++) {
-    buttonArray[i].addEventListener("click", function() {
-        var user = blockArray.value;
-        localStorage.setItem("user", JSON.stringify(user));
-    });
-};
+$("button").on("click", function() {
+    var newVariable = $(this).parent().siblings("textarea").val().trim();
+    var newId = $(this).parent().parent().attr("id");
+    localStorage.setItem(newId, newVariable);
+});
